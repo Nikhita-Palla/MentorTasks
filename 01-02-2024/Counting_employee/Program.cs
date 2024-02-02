2222ed8470d673b9e2a3c7ad0c82554c88d0341c
Nikhita-Palla <pallanikhita@gmail.com>
2024-02-02 14:24:48 +0530

    Added 01-02-2024 Assignment

---
+++ 01-02-2024/Counting_employee/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Counting_employee
+{
+    class Employee
+    {
+        static int count = 0;
+        string firstName;
+        string lastName;
+        float salary;
+        string address;
+        public Employee(string firstName, string lastName, float salary, string address)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+            this.address = address;
+
+            count = count + 1;
+        }
+        public static int countEmployees()
+        {
+            return count;
+        }
+    }
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Employee e1 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e2 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e3 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e4 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e5 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e6 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e7 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e8 = new Employee("Nikhita", "Palla", 30000, "VZM");
+            Employee e9 = new Employee("Nikhita", "Palla", 30000, "VZM");
+
+            Console.WriteLine(Employee.countEmployees());
+
+        }
+    }
+
+}
---
+++ 01-02-2024/this_keyword/Program.cs
@@ -0,0 +1,34 @@
+﻿class Calculation
+{
+    private int a;
+    private int b;
+    public Calculation():this(1,2)
+    {
+        
+    }
+    public Calculation(int a,int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    
+    public int getA()
+    {
+        return a;
+    }
+    public int getB()
+    {
+        return b;
+    }
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Calculation c = new Calculation();
+        Console.WriteLine(c.getA());
+        Console.WriteLine(c.getB());
+
+    }
+}
